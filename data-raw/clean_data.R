cbeb3c0a7162acf7ebea2b77dbb6a59a92c7a41c
Badhia <byuneskatz@flowwest.com>
2024-03-11 15:57:33 -0700

    drafts clean_data.R, makes changes to medatada field names

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,4 +1,68 @@
 library(tidyverse)
+library(googleCloudStorageR)
 
+
+# google cloud set up
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+
+# get data from google cloud
+gcs_get_object(object_name = "juvenile-rearing-monitoring/seine-and-snorkel-data/feather-river/data/combined_feather_snorkel_data.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk =  here::here("data-raw", "combined_feather_snorkel_data.csv"),
+               overwrite = TRUE)
+
+combined_snorkel <- read_csv(here::here("data-raw", "combined_feather_snorkel_data.csv"))
+
+
+summary(combined_snorkel)
+glimpse(combined_snorkel)
+#character variables
+unique(combined_snorkel$weather)
+unique(combined_snorkel$section_name)
+unique(combined_snorkel$section_type)
+unique(combined_snorkel$units_covered)
+unique(combined_snorkel$unit)
+unique(combined_snorkel$size_class)
+unique(combined_snorkel$substrate) #decoded for fields that only had one value, those with more than 1 stayed as code
+unique(combined_snorkel$instream_cover) #decoded for fields that only had one value, those with more than 1 stayed as code
+unique(combined_snorkel$hydrology)
+unique(combined_snorkel$run)
+unique(combined_snorkel$tagged)
+unique(combined_snorkel$clipped)
+unique(combined_snorkel$overhead_cover)
+unique(combined_snorkel$location)
+unique(combined_snorkel$survey_type)
+unique(combined_snorkel$section_type)
+unique(combined_snorkel$species)
+unique(combined_snorkel$unit_type) #check decoding system for this field since it is the same code as hydrology
+
+
+#numeric variables
+summary(combined_snorkel$survey_id)
+range(combined_snorkel$date)
+summary(combined_snorkel$flow)
+summary(combined_snorkel$section_number)
+summary(combined_snorkel$turbidity)
+summary(combined_snorkel$temperature)
+head(combined_snorkel$end_time[5:10])
+head(combined_snorkel$start_time[5:10])
+summary(combined_snorkel$count)
+summary(combined_snorkel$est_size)
+summary(combined_snorkel$water_depth_m)
+summary(combined_snorkel$fork_length)
+summary(combined_snorkel$bank_distance) #6412 of this values are NA, TODO check if we want to keep
+summary(combined_snorkel$visibility)
+summary(combined_snorkel$section_number)
+
+
+
+
+
+# write files -------------------------------------------------------------
+write.csv(carcass_estimate, here::here("data-raw", "combined_feather_snorkel_data.csv"), row.names = FALSE)
+
+# save cleaned data to `data/`
+read.csv(here::here("data-raw", "combined_feather_snorkel_data.csv")) |> glimpse()
 
 # save cleaned data to `data/`
