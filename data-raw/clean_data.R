b46133a3e0e561154c5de11daf3268aaaca4a7be
Badhia <byuneskatz@flowwest.com>
2024-04-01 18:21:53 -0700

    delets addressed todo's and adds needed comments, validates xml

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -26,10 +26,10 @@ glimpse(combined_snorkel)
 unique(combined_snorkel$weather)
 unique(combined_snorkel$section_name)
 unique(combined_snorkel$section_type)
-unique(combined_snorkel$units_covered) #check field meaning
+unique(combined_snorkel$units_covered)
 unique(combined_snorkel$unit) #check field meaning
 unique(combined_snorkel$size_class)
-unique(combined_snorkel$instream_cover) #there is a code reference for this field however, it would only work for those fields that had one value. There were fields with more than 1 value. Decided to skip decode
+unique(combined_snorkel$instream_cover) #there is a code reference for this field, multiple codes at once in some fields
 unique(combined_snorkel$hydrology)
 unique(combined_snorkel$run)
 unique(combined_snorkel$tagged)
@@ -37,9 +37,9 @@ unique(combined_snorkel$clipped)
 unique(combined_snorkel$overhead_cover)
 unique(combined_snorkel$location)
 unique(combined_snorkel$survey_type) #check field meaning
-unique(combined_snorkel$section_type) #check field meaning
+unique(combined_snorkel$section_type)
 unique(combined_snorkel$species)
-unique(combined_snorkel$unit_type) #check decoding system for this field since it is the same code as hydrology
+unique(combined_snorkel$unit_type) #still unsure about different between this field and hydrology
 
 #numeric variables ----
 summary(combined_snorkel$survey_id)
@@ -64,18 +64,17 @@ head(combined_snorkel$start_time[5:10])
 summary(combined_snorkel$count)
 ggplot(combined_snorkel, aes(count)) +
   geom_histogram()
-summary(combined_snorkel$est_size) #check field meaning
+summary(combined_snorkel$est_size)
 summary(combined_snorkel$water_depth_m)
 ggplot(combined_snorkel, aes(water_depth_m)) +
   geom_histogram()
 summary(combined_snorkel$fork_length)
 ggplot(combined_snorkel, aes(fork_length)) +
   geom_histogram()
-summary(combined_snorkel$bank_distance) #96.6% of this values are NA, TODO check if we want to keep, check description on metadata
-summary(combined_snorkel$visibility) #check for field meaning
+summary(combined_snorkel$bank_distance)
+summary(combined_snorkel$visibility)
 summary(combined_snorkel$section_number)
-unique(combined_snorkel$substrate) #there is a code reference for this field however, it would only work for those fields that had one value. There were fields with more than 1 value. Decided to skip decode
-#keeping substrate as character for now since numbers are referring to code
+unique(combined_snorkel$substrate) #keeping substrate as character since numbers are referring to code, multiple codes at once in some fields
 
 
 # write files -------------------------------------------------------------
