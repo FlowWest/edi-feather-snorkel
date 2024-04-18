5113457186e84acd20633dd2e0d3fb0e800c396f
Erin Cain <ecain@flowwest.com>
2024-04-18 09:42:12 -0700

    update/clean up data package

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(googleCloudStorageR)
+source("data-raw/metadata/species_lookup.R")
 
 
 # google cloud set up
@@ -12,7 +13,7 @@ gcs_get_object(object_name = "juvenile-rearing-monitoring/seine-and-snorkel-data
                saveToDisk =  here::here("data-raw", "combined_feather_snorkel_data.csv"),
                overwrite = TRUE)
 
-combined_snorkel <- read_csv(here::here("data-raw", "combined_feather_snorkel_data.csv"))
+combined_snorkel <- read_csv(here::here("data-raw", "combined_feather_snorkel_data.csv")) |> glimpse()
 
 # clean data --------------------------------------------------------------
 # here is where we clean up the data and make sure it all looks as expected
@@ -22,63 +23,228 @@ combined_snorkel <- read_csv(here::here("data-raw", "combined_feather_snorkel_da
 summary(combined_snorkel)
 glimpse(combined_snorkel)
 
+# WEATHER ----------------------------------------------------------------------
 #character variables ----
-unique(combined_snorkel$weather)
+unique(combined_snorkel$weather) # fine as is
+
+# SECTION NAME -----------------------------------------------------------------
+# TODO Section name updates/questions
 unique(combined_snorkel$section_name)
-unique(combined_snorkel$section_type)
+# - we updated Bigriffle to be Big Riffle, is that accurate?)
+# - ? what to do about the section names that include multiple "hatchery and mo's riffles"
+# - do you have a list of ones you currently use? Do you have a map of sites?
+
+# SECTION TYPE -----------------------------------------------------------------
+# TODO ?
+unique(combined_snorkel$section_type) # what does this mean? Do we need to keep? should we only use permanant sections for analysis
+# all permanent and random sectin types have NA section names and numbers...not useful
+# Suggest that we remove
+combined_snorkel |> filter(section_type == "permanent") |> pull(section_name) |> unique()
+combined_snorkel |> filter(section_type == "permanent") |> pull(section_number) |> unique()
+
+combined_snorkel |> filter(section_type == "random") |> pull(section_name) |> unique()
+combined_snorkel |> filter(section_type == "random") |> pull(section_number) |> unique()
+
+
+combined_snorkel |> filter(is.na(section_type)) |> pull(section_name) |> unique()
+combined_snorkel |> filter(is.na(section_type)) |> pull(section_number) |> unique()
+
+# UNITS COVERED -----------------------------------------------------------------
+# TODO can we remove? seems more like metadata then data for a given reccord
+# # are these ones covered in a survey? do we need both this and unit"
+# Suggest removing
 unique(combined_snorkel$units_covered)
-unique(combined_snorkel$unit) #check field meaning
+
+combined_snorkel |>
+  mutate(has_units_covered = ifelse(is.na(units_covered), FALSE, TRUE)) |>
+  ggplot(aes(x = date, y = has_units_covered)) +
+  geom_point()
+
+# UNIT -----------------------------------------------------------------
+# TODO What is the definition of this?
+unique(combined_snorkel$unit) #check field meaning, why can there be two units?
+
+combined_snorkel |>
+  mutate(has_units = ifelse(is.na(unit), FALSE, TRUE)) |>
+  ggplot(aes(x = date, y = has_units)) +
+  geom_point()
+
+# size class -----------------------------------------------------------------
+# TODO ?
+#  What is this? Fish or other size class? do you have the lookup for this? What do these correspond to?
 unique(combined_snorkel$size_class)
-unique(combined_snorkel$instream_cover) #there is a code reference for this field, multiple codes at once in some fields
+
+# instream cover -----------------------------------------------------------------
+unique(combined_snorkel$instream_cover) # confirm metadata has code lookup
+
+# hydrology & unit type -----------------------------------------------------------------
 unique(combined_snorkel$hydrology)
-unique(combined_snorkel$run)
-unique(combined_snorkel$tagged)
-unique(combined_snorkel$clipped)
-unique(combined_snorkel$overhead_cover)
-unique(combined_snorkel$location)
-unique(combined_snorkel$survey_type) #check field meaning
-unique(combined_snorkel$section_type)
-unique(combined_snorkel$species)
+# TODO used unit type pre 2005 and hydrology post, called them hydrology
 unique(combined_snorkel$unit_type) #still unsure about different between this field and hydrology
+sum(combined_snorkel$unit_type == combined_snorkel$hydrology, na.rm = TRUE)
+
+combined_snorkel |> ggplot(aes(x = date, y = hydrology, color = hydrology)) +
+  geom_point()
+combined_snorkel |> ggplot(aes(x = date, y = unit_type, color = unit_type)) +
+  geom_point()
+
+# run -----------------------------------------------------------------
+unique(combined_snorkel$run)
+
+# tagged -----------------------------------------------------------------
+unique(combined_snorkel$tagged)
+
+# clipped -----------------------------------------------------------------
+# TODO - are there clipped trout? If so is this RBTC? Assumed that but can remove?
+unique(combined_snorkel$clipped)
+
+# overhead cover -----------------------------------------------------------------
+unique(combined_snorkel$overhead_cover)
+
+# location -----------------------------------------------------------------
+# TODO questions?
+# how is this different from section name, seems maybe like it is more specific
+unique(combined_snorkel$location)
+
+# survey_type -----------------------------------------------------------------
+unique(combined_snorkel$survey_type) #check field meaning, no response in email on what comp was, PROPOSE REMOVE (OKAY?)
+
+# species -----------------------------------------------------------------
+# Mapping to lookup codes from database in cleaned table below
+unique(combined_snorkel$species)
+
+
+# substrate --------------------------------------------------------------------------
+unique(combined_snorkel$substrate) #keeping substrate as character since numbers are referring to code, multiple codes at once in some fields
+
+# survey_id --------------------------------------------------------------------------
 
 #numeric variables ----
 summary(combined_snorkel$survey_id)
+
+# Date --------------------------------------------------------------------------
+# TODO questions
+# we are missing 2021 - 2024, should we get this data to add in?
 range(combined_snorkel$date)
+
+# Flow --------------------------------------------------------------------------
 summary(combined_snorkel$flow)
 ggplot(combined_snorkel, aes(flow)) +
   geom_histogram()
 combined_snorkel$flow <- ifelse(combined_snorkel$flow == 0, NA, combined_snorkel$flow) |> #changing flow values from 0 to NA
   glimpse()
+
+# section number --------------------------------------------------------------------------
+# TODO - we created this field based on section number in DMP, is this accurate mapping
+# section_number = case_when(section_name == "Aleck Riffle" ~ 8,
+# section_name == "Auditorium Riffle" ~ 4,
+# section_name == "Bedrock Park Riffle" ~ 5,
+# section_name == "Bedrock Riffle" ~ 10,
+# section_name == "Big Riffle" ~ 17,
+# section_name == "Eye Riffle" ~ 11,
+# section_name == "G95" ~ 14,
+# section_name == "Gateway Riffle" ~ 12,
+# section_name == "Goose Riffle" ~ 16,
+# section_name == "Gridley Riffle" ~ 19,
+# section_name == "Hatchery Ditch" ~ 2,
+# section_name == "Hatchery Riffle" ~ 1,
+# section_name == "Junkyard Riffle" ~ 20,
+# section_name == "Kiester Riffle" ~ 15,
+# section_name == "Matthews Riffle" ~ 7,
+# section_name == "McFarland" ~ 18,
+# section_name == "Mo's Ditch" ~ 3,
+# section_name == "Robinson Riffle" ~ 9,
+# section_name == "Steep Riffle" ~ 10,
+# section_name == "Trailer Park Riffle" ~ 6,
+# section_name == "Vance Riffle" ~ 13,
+# TRUE ~ NA))
 summary(combined_snorkel$section_number)
+
+# Turbidity --------------------------------------------------------------------------
 summary(combined_snorkel$turbidity)
 ggplot(combined_snorkel, aes(turbidity)) +
   geom_histogram()
+
+# Temp --------------------------------------------------------------------------
 summary(combined_snorkel$temperature)
 ggplot(combined_snorkel, aes(x = date, y = temperature)) +
   geom_point()
 combined_snorkel$temperature <- ifelse(combined_snorkel$temperature == 0, NA, combined_snorkel$temperature) |> #changing flow values from 0 to NA since they are potential outliers
   glimpse()
 
+# Time (start and end) --------------------------------------------------------------------------
+
 head(combined_snorkel$end_time[5:10])
 head(combined_snorkel$start_time[5:10])
 summary(combined_snorkel$count)
 ggplot(combined_snorkel, aes(count)) +
   geom_histogram()
+
+# Est size --------------------------------------------------------------------------
+# TODO - size and fork length? do we need to keep them both, which one should we keep
+# they do not have quite the same distribution
 summary(combined_snorkel$est_size)
+ggplot(combined_snorkel, aes(est_size, fill = species)) +
+  geom_histogram()
+
+# Fork length --------------------------------------------------------------------------
+summary(combined_snorkel$fork_length)
+ggplot(combined_snorkel, aes(fork_length, fill = species)) +
+  geom_histogram()
+
+# Date --------------------------------------------------------------------------
 summary(combined_snorkel$water_depth_m)
 ggplot(combined_snorkel, aes(water_depth_m)) +
   geom_histogram()
-summary(combined_snorkel$fork_length)
-ggplot(combined_snorkel, aes(fork_length)) +
-  geom_histogram()
+
+# Bank distance  --------------------------------------------------------------------------
+# TODO what are the units here?
 summary(combined_snorkel$bank_distance)
+
+# visibility --------------------------------------------------------------------------
 summary(combined_snorkel$visibility)
-summary(combined_snorkel$section_number)
-unique(combined_snorkel$substrate) #keeping substrate as character since numbers are referring to code, multiple codes at once in some fields
+
+
+
+cleaned_combined_snorkel <- combined_snorkel |>
+  mutate(instream_cover = toupper(instream_cover),
+         hydrology = ifelse(year(date) > 2005, unit_type, hydrology)) |>
+  select(-unit_type) |>
+  left_join(species_lookup, by = c("species" = "OrganismCode")) |>
+  select(-species) |>
+  rename(species = CommonName) |>
+  select(-c(Order1, Family, Genus, Species)) |>
+  mutate(run = case_when(species == "Chinook Salmon- Fall" ~ "fall",
+                        species == "Chinook Salmon- Late Fall" ~ "late fall",
+                        species %in% c("Chinook Salmon - Spring", "Chjnook Salmon- Spring") ~ "spring",
+                        TRUE ~ NA_character_),
+         clipped = case_when(species == "Steelhead Trout (ad clipped)" ~ TRUE,
+                             species == "Rainbow Trout (wild)" ~ FALSE,
+                             TRUE ~ clipped),
+         species = str_to_title(case_when(species %in% c("Chinook Salmon- Fall",
+                                         "Chinook Salmon- Late Fall",
+                                         "Chinook Salmon- Spring",
+                                         "Chjnook Salmon- Spring") ~ "Chinook Salmon",
+                             species =="Rainbow Trout (wild)" ~ "Rainbow Trout",
+                             species == "Steelhead Trout (ad clipped)" ~ "Steelhead Trout",
+                             species == "Unid Juvenile Sculpin" ~ "Unidentified Juvenile Sculpin",
+                             species == "Unid Juvenile Bass (Micropterus sp.)" ~ "Unidentified Juvenile Bass",
+                             species == "Unid Juvenile Lamprey" ~ "Unidentified Juvenile Lamprey",
+                             species == "Unid Juvenile Minnow" ~ "Unidentified Juvenile Minnow",
+                             species == "UNID Sunfish"   ~ "Unidentified Sunfish",
+                             species == "Unid Juvenile non-Micropterus Sunfish" ~ "Unidentified Juvenile non-Micropterus Sunfish",
+                             species == "Unid Juvenile Fish" ~ "Unidentified Juvenile Fish",
+                             species == "NO FISH CAUGHT" ~ NA,
+                             TRUE ~ species))) |> glimpse()
+
+cleaned_combined_snorkel$species |> unique()
+
+# consider additionally removing survey_type, est_size, units_covered, location, bank_dist (more than 95% na, no longer used)
+# TODO Questions: Additional spatial considerations - Is there a location table that would map units to sections/section type, ect?
 
 
 # write files -------------------------------------------------------------
 
 # save cleaned data to `data/`
-write.csv(combined_snorkel, here::here("data", "combined_feather_snorkel_data.csv"), row.names = FALSE)
+write_csv(combined_snorkel, here::here("data", "combined_feather_snorkel_data.csv"))
 
