632cb0f015d5371d12620f171c175c5f4b0f560f
alvizek <avizek@flowwest.com>
2024-08-19 16:06:31 -0700

    updates to abstract, methods to md

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -34,7 +34,7 @@ metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x)
 names(metadata) <- sheets
 
 abstract_docx <- "data-raw/metadata/abstract.docx"
-methods_docx <- "data-raw/metadata/methods.docx"
+methods_docx <- "data-raw/metadata/methods.md"
 
 #edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"), password = Sys.getenv("EDI_PASSWORD"))
 edi_number <- "feather-snorkel"
